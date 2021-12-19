3249e095520027db855a71b3922cf5a96f96eceb
dean <dean.w.mcbean@gmail.com>
2021-12-19 20:53:26 +0000

    updated data & removed braeburn

--- js/data-load.js
+++ js/data-load.js
@@ -10,12 +10,11 @@ import_manager.addImport('growth_statistics', 'Growth Statistics', 'csv',
 
 import_manager.onComplete(importsComplete);
 import_manager.runImports();
-console.log(import_manager);
 
 var growth_statistics = [];
 
 function importsComplete(imports) {
-    growth_statistics = imports['growth_statistics'].filter(d => d.year != "2015" && !d.rpin.startsWith("OLD BLOCK"));
+    growth_statistics = imports['growth_statistics'].filter(d => d.year != "2015" && !d.rpin.startsWith("OLD BLOCK") && d.variety != 'braeburn');
     
     init();
 }
